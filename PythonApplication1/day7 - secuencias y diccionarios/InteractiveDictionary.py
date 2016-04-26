5630dd8c4e5bf1d9d0aa83ec2c0c97c2cc306071
dkrynve <dkrynve@gmail.com>
2016-04-26 12:33:00 -0300

    Practicando con dictionarios

---
+++ PythonApplication1/day7 - secuencias y diccionarios/InteractiveDictionary.py
@@ -0,0 +1,11 @@
+import os
+
+my_dict = {'auto':'fiesta','avion':'boing','color':'blanco','color':'gris','a':'1','b':'2',}
+
+for i,j in my_dict.items():
+    print(i + ":" + j)
+
+
+print (my_dict.values())
+
+print (my_dict.keys())
